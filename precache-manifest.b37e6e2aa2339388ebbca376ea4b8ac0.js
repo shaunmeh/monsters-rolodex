c2c0ff2da980222e55f598609f3c0de571f46c75
shaunmeh <50548709+shaunmeh@users.noreply.github.com>
2020-04-01 16:55:32 -0700

    Updates

---
+++ precache-manifest.b37e6e2aa2339388ebbca376ea4b8ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec241b7c7e9bb3e60bf17a6ba98b5360",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7c7611f964ed52ef6f81",
+    "url": "/monsters-rolodex/static/css/main.25c73629.chunk.css"
+  },
+  {
+    "revision": "715e0d2bfd76f6d44613",
+    "url": "/monsters-rolodex/static/js/2.c134a042.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.c134a042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7611f964ed52ef6f81",
+    "url": "/monsters-rolodex/static/js/main.0d319cfb.chunk.js"
+  },
+  {
+    "revision": "3c48bf0967bd06c12b37",
+    "url": "/monsters-rolodex/static/js/runtime-main.a041b21b.js"
+  }
+]);
